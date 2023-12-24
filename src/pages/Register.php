9fd82991f1eca6c7b2b2cde2a8b524cc749448ce
WorkerHarder171 <dadybima171@gmail.com>
2023-12-24 20:52:20 +0700

    feat(Login & Register): push slicing UI Login and Register

---
+++ src/pages/Register.php
@@ -0,0 +1,30 @@
+<div class="wrapper vh-100 d-flex flex-column justify-content-center align-items-center">
+    <div class="cards border px-5 py-3 rounded-lg" style="width: 30%;">
+        <div class="text-group my-2 d-flex flex-column justify-content-center align-items-center">
+            <img src="src/asset_files/udinus_logo.jpg" class="text-center w-25 my-2" alt="">
+            <h3 class="text-center text-capitalize">Buat Akun</h3>
+            <p class="text-center text-secondary">Masukkan informasi berdasarkan form</p>
+        </div>
+        <form>
+            <div class="form-group my-2 gap-3">
+                <div class="d-flex justify-content-evenly align-items-center" style="gap: 0.5em;">
+                    <input type="text" class="w-100 px-4 py-3 my-3 rounded-lg border page-link" id="username" placeholder="Nama depan" autocomplete="off">
+                    <input type="text" class="w-100 px-4 py-3 my-3 rounded-lg border page-link" id="password" placeholder="Nama belakang" autocomplete="off">
+                </div>
+                <input type="date" class="w-100 px-4 py-3 my-3 rounded-lg border page-link text-secondary" id="date" autocomplete="off">
+                <input type="email" class="w-100 px-4 py-3 my-3 rounded-lg border page-link" id="email" placeholder="Email" autocomplete="off">
+                <div class="d-flex justify-content-evenly align-items-center" style="gap: 0.5em;">
+                    <input type="password" class="w-100 px-4 py-3 my-3 rounded-lg border page-link" id="password" placeholder="Password" >
+                    <input type="password" class="w-100 px-4 py-3 my-3 rounded-lg border page-link" id="re-password" placeholder="Re-Password" >
+                </div>
+
+            </div>
+            <div class="form-group d-flex justify-content-end align-items-center">
+                <button type="submit" class="w-auto px-4 btn btn-primary btn-block rounded-2">Selanjutnya</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+<div class="wrapper"></div>
